4336bc036d7155b4ee06f7893b02843b455c14fe
Marc Molina Torres <marcmolina_10@hotmail.com>
2018-03-07 18:03:39 +0100

    Palas y pelota funcinales juento a las paredes

---
+++ Joc_MJA/Assets/Scripts/Ball.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody>().velocity = Vector3.left * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "pala")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector3 dir = new Vector3(1, y, 0).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "pala2")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector3 dir = new Vector3(-1, y, 0).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+
+
+
+        }
+
+        if (col.gameObject.name == "paretL")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            //Vector3 dir = new Vector3(1, -GetComponent<Rigidbody>().velocity.y, 0).normalized;
+
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            if (GetComponent<Rigidbody>().velocity.y < 0f)
+            {
+                dir = new Vector3(1, -1, 0).normalized;
+            }
+            else if (GetComponent<Rigidbody>().velocity.y > 0)
+            {
+                dir = new Vector3(1, 1, 0).normalized;
+            }
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "paretR")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            //Vector3 dir = new Vector3(1, -GetComponent<Rigidbody>().velocity.y, 0).normalized;
+
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            if (GetComponent<Rigidbody>().velocity.y < 0f)
+            {
+                dir = new Vector3(-1, -1, 0).normalized;
+            }
+            else if (GetComponent<Rigidbody>().velocity.y > 0)
+            {
+                dir = new Vector3(-1, 1, 0).normalized;
+            }
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "paretT")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            //Vector3 dir = new Vector3(1, -GetComponent<Rigidbody>().velocity.y, 0).normalized;
+
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            if (GetComponent<Rigidbody>().velocity.x < 0f)
+            {
+                dir = new Vector3(-1, -1, 0).normalized;
+            }
+            else if(GetComponent<Rigidbody>().velocity.x > 0)
+            {
+                dir = new Vector3(1, -1, 0).normalized;
+            }
+           
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "paretD")
+        {
+            // Calculate hit Factor
+            float y = HitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            //Vector3 dir = new Vector3(1, -GetComponent<Rigidbody>().velocity.y, 0).normalized;
+
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            if (GetComponent<Rigidbody>().velocity.x < 0f)
+            {
+                dir = new Vector3(-1, 1, 0).normalized;
+            }
+            else if (GetComponent<Rigidbody>().velocity.x > 0)
+            {
+                dir = new Vector3(1, 1, 0).normalized;
+            }
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody>().velocity = dir * speed;
+        }
+    }
+}
---
+++ Joc_MJA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public bool player1;
+    public float speed;
+    private Rigidbody RB;
+    public  float heightPala;
+
+    // Use this for initialization
+    void Start() {
+        RB = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player1) { movementPlayer1(); }
+        if (!player1) { movementPlayer2(); }
+        checkPositions();
+
+    }
+
+    public void movementPlayer1()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, RB.transform.position.y + speed, RB.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, RB.transform.position.y - speed, RB.transform.position.z);
+        }
+    }
+
+    public void movementPlayer2()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, RB.transform.position.y + speed, RB.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, RB.transform.position.y - speed, RB.transform.position.z);
+        }
+
+    }
+
+    public void checkPositions()
+    {
+        if(RB.transform.position.y > heightPala)
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, heightPala, RB.transform.position.z);
+        }
+
+        if (RB.transform.position.y < -heightPala)
+        {
+            RB.transform.position = new Vector3(RB.transform.position.x, -heightPala, RB.transform.position.z);
+        }
+    }
+}
